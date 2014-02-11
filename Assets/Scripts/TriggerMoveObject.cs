a4cd1515c079dfed935e255b64502680f0cbdabe
Andrew Morris <jump3r@dggaming.net>
2014-02-11 01:46:08 -0500

    Added First Move Test
    Added Script that moves a cube based off of a trigger

---
+++ Assets/Scripts/TriggerMoveObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerMoveObject : MonoBehaviour {
+
+	public Vector3 pos1 = new Vector3(9, 2, 20);
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col) {
+		GameObject.Find ("FirstCube").transform.position = pos1; // Find the GameObject
+	}
+}
