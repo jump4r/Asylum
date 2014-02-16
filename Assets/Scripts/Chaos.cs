a98a0579df5eec33e9c4c628b99a92f21fac8448
Andrew Morris <jump3r@dggaming.net>
2014-02-16 01:24:06 -0500

    Changed project to Markus' Prototype.
    Changed the prototype, added support to "Event Trigger Zones"

---
+++ Assets/Scripts/Chaos.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chaos : MonoBehaviour {
+
+	private GameObject crazyObjs;
+	
+	void Awake ()
+	{
+		crazyObjs = GameObject.Find ("CrazyObjs");
+		//print (crazyObjs.transform.childCount);
+
+	}
+
+	/* check if a given point in 3d space is within the player's FOV */
+	bool WithinFOV(Vector3 point)
+	{
+		Vector3 diff = point - Camera.main.transform.position;
+		diff.Normalize();
+		//print ("start:");
+		//print (		Vector3.Dot(diff, Camera.main.transform.forward));
+		//print (Mathf.Deg2Rad*Camera.main.fieldOfView);
+		return ( Vector3.Dot(diff, Camera.main.transform.forward) >= Mathf.Cos(Mathf.Deg2Rad*Camera.main.fieldOfView));
+	}
+	
+	/* update the player's observations */
+	void Update () {
+
+	}
+
+	void Change () {
+		//Debug.Log ("Change called!");
+		//TODO: We can probably move this to the Awake() function
+		Transform[] allCrazies = crazyObjs.GetComponentsInChildren<Transform>();
+		
+		// mark objects as observed / not observed 
+		foreach (Transform t in allCrazies)
+		{
+			if(t.name == "CrazyObjs" || t.name == "ChaosTrigger")
+				continue;
+
+			Debug.Log(t.name);
+			ObjActions temp = t.gameObject.GetComponent<ObjActions>();
+			temp.isObserved = WithinFOV(t.position);
+		}
+	}
+
+	void OnTriggerStay(Collider col) {
+		//Debug.Log (col.name);
+		if (col.name == "ChaosTrigger") {
+			Change ();
+		}
+	}
+}
---
+++ Assets/Scripts/ObjActions.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjActions : MonoBehaviour {
+
+	public bool isObserved = true;
+	private bool hasChanged = false;
+	private Vector3 normalScale;
+
+	/* initialize or save anything we need here */
+	void Awake()
+	{
+		normalScale = transform.localScale;
+	}
+
+	/* randomly change the scale of the object within reason */
+	void changeScale()
+	{
+		if(normalScale != transform.localScale)
+			transform.localScale = normalScale;
+
+		transform.localScale += (new Vector3(Random.Range (.2f, 2f), Random.Range (.2f, 2f), Random.Range (.2f, 2f)));
+		//print (transform.localScale);
+	}
+
+	
+	void Update ()
+	{
+
+		/* if the player is watching the object*/
+		if(isObserved)
+		{
+			//print (transform.name + " is being observed");
+			hasChanged = false;
+			return;
+		}
+
+		/* otherwise do silly stuff when nobody is looking */
+		if(!hasChanged)
+		{
+			changeScale();
+
+			/* add more silly stuff here */
+
+			/* make sure we tell the object not do more silly stuff next time */
+			hasChanged = true;
+		}
+	}
+}
