ac042a5e14e3392f91bb306b13ff852a61b68f59
Andrew Morris <jump3r@dggaming.net>
2014-02-16 03:30:43 -0500

    ACTUALLY Added Support for .txt descriptions this time instead of being a scrub.
    Appearntly I saved my stuff to the wrong folder cause I'm a scrub.

---
+++ Assets/Scripts/ObjectDescriptor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectDescriptor : MonoBehaviour {
+
+	public TextAsset desc;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/TriggerObjGUI.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerObjGUI : MonoBehaviour {
+
+	private bool active_GUI = false;
+	//private ObjectDescriptor child_script = GetComponentInChildren<ObjectDescriptor>();
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col) {
+		Debug.Log (col.name);
+		if (col.name == "First Person Controller") {
+			active_GUI = true;
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+		//Debug.Log (col.name);
+		if (col.name  == "First Person Controller") {
+			active_GUI = false;
+		}
+	}
+
+	void DisplayText () {
+
+	}
+
+	void OnGUI() {
+		if (active_GUI) {
+			ObjectDescriptor child_script = GetComponentInChildren<ObjectDescriptor>(); // Get the 
+			string desc = child_script.desc.ToString();
+
+			Debug.Log (desc);
+			GUI.Box (new Rect(Screen.width * .3f, Screen.height * .9f, Screen.width *.4f, 40f), desc);
+		}
+	}
+}
