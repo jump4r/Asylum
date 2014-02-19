a07fa3c08e2627aebb0634e42d95ce5c25d08123
Andrew Morris <jump3r@dggaming.net>
2014-02-19 01:24:37 -0500

    Added Scrolling Text
    Text now scrolls through the dialog (See the second piece of dialog), however I have no idea how to time it. I'll look into more tomorrow, but it doesn't seem like an easy feat.
    Also, known bug: Alt-Tabbing the game will remove the subtitles.

--- Assets/Scripts/Conversation.cs
+++ Assets/Scripts/Conversation.cs
@@ -8,17 +8,23 @@ public class Conversation : MonoBehaviour{
 
 	private bool active_speech;
 
-	private int sub_switch;	// Used for subtitles.
-	private string current_sub;
+	private float sub_switch;	// Used for subtitles.
+	private int current_sub;
+	private string[] subtitle;
+	private bool print_two_lines;
 	// Use this for initialization
 
 	void Start () {
 		active_speech = false;
 		sub_switch = 0;
+		current_sub = 0;
 	}
 
 	public void Setup(string tag) {	// Setup with the correct conversation based off of the tag.
 		Inmate inm = GameObject.FindGameObjectWithTag (tag).GetComponent<Inmate> ();
+		sub_switch = 0;
+		current_sub = 0;
+		print_two_lines = true;
 		Debug.Log (inm.GetCurrentConv ());
 		if (inm.GetCurrentConv () >= inm.conv_clips.Length) { // If out of bounds, set the clips to 0, as that will be the default.
 			conv_clip = inm.conv_clips [0];
@@ -28,6 +34,10 @@ public class Conversation : MonoBehaviour{
 			conv_clip = inm.conv_clips [inm.GetCurrentConv ()];
 			conv_text = inm.conv_texts [inm.GetCurrentConv ()];
 		}
+		subtitle = conv_text.text.Split ("\n" [0]);
+		if (subtitle.Length <= 1) {
+			print_two_lines = false;
+		}
 	}
 	
 	// Update is called once per frame
@@ -36,8 +46,15 @@ public class Conversation : MonoBehaviour{
 			active_speech = false;
 			EndConversation();
 		} 
+
 		else if (active_speech) {
-			// Get subtitles to rotate with text, I will do this in class (2-19).
+			sub_switch += Time.deltaTime;
+			if (sub_switch > 6 && current_sub < subtitle.Length-1) { // Literally the most editable number in the game.
+				sub_switch = 0;
+				current_sub += 2;
+				if (current_sub >= subtitle.Length)
+					print_two_lines = false;
+			}
 		}
 	}
 
@@ -56,8 +73,11 @@ public class Conversation : MonoBehaviour{
 
 	void OnGUI() {
 		if (active_speech) {
-			current_sub = conv_text.ToString();
-			GUI.Box (new Rect(Screen.width * .3f, Screen.height * .9f, Screen.width *.4f, 40f), current_sub);
+			//current_sub = conv_text.ToString();
+			if (print_two_lines)
+				GUI.Box (new Rect(Screen.width * .3f, Screen.height * .9f, Screen.width *.4f, 40f), subtitle[current_sub] + "\n" + subtitle[current_sub+1]);
+			else 
+				GUI.Box (new Rect(Screen.width * .3f, Screen.height * .9f, Screen.width *.4f, 40f), subtitle[current_sub]);
 		}
 	}
 }
