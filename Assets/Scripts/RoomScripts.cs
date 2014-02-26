768e9bc8337a235ef27fcec00f4e80e1738b2fa6
Markus Gaasedelen <gaasem@rpi.edu>
2014-02-26 06:08:46 -0500

    Trigger zones are working, door 'slams' when player is 'deep' within the room. Ready for additional scripting and whatnot.

--- Assets/Scripts/RoomScripts.cs
+++ Assets/Scripts/RoomScripts.cs
@@ -10,14 +10,20 @@ public class RoomScripts : MonoBehaviour {
 
 	void Awake()
 	{
-		deep = transform.FindChild("deep").GetComponent<RoomTrigger>();
-		within = transform.FindChild("within").GetComponent<RoomTrigger>();
-		door = transform.FindChild("Door").gameObject;
-	}
 
-	// Use this for initialization
-	void Start () {
-	
+		/* locate our different zones for this room */
+		RoomTrigger[] temp = GetComponentsInChildren<RoomTrigger>();
+		foreach(RoomTrigger zone in temp)
+		{
+			if(zone.name == "deep")
+				deep = zone;
+			else if (zone.name == "within")
+				within = zone;
+		}
+
+		//TODO: change this to search only the local object...
+		door = GameObject.Find ("Door");
+		door.SetActive(false);
 	}
 	
 	/* update the state of the room */
