a4272b045e1052b4cb84e2c68ee95ef458fcc447
markus <gaasem@rpi.edu>
2014-03-04 23:22:12 -0500

    FBXScaleFix makes sure the editor / model importer imports stuff at scale 1.0

---
+++ Assets/Editor/FBXScaleFix.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+
+public class FBXScaleFix : AssetPostprocessor {
+
+	// Use this for initialization
+	public void OnPreprocessModel()
+	{
+		ModelImporter modelImporter = (ModelImporter)assetImporter;
+		modelImporter.globalScale = 1;
+	}
+}
