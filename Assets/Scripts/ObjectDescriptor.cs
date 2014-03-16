313f405df2a709de1f19ee1cae37db6d700c240c
jump4r <jump3r@dggaming.net>
2014-03-16 03:19:43 -0400

    Interactivity Progress & Level Design.  - Interactivity with Stationary Objects with and without audio.  - Base Interactions with doors is being set up
    Updated Level a bit by adding some things, but this is still just a test. Lets hope I didn't break anything.

--- Assets/Scripts/ObjectDescriptor.cs
+++ Assets/Scripts/ObjectDescriptor.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ObjectDescriptor : MonoBehaviour {
 
 	public TextAsset desc;
+	// public AudioClip clip;
 
 	// Use this for initialization
 	void Start () {
--- Assets/Scripts/TriggerObjAudioGUI.cs
+++ Assets/Scripts/TriggerObjAudioGUI.cs
@@ -1,42 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TriggerObjGUI : MonoBehaviour {
+public class TriggerObjAudioGUI : MonoBehaviour {
 
 	private bool active_GUI = false;
+	private bool active_playing = false;
 	//private ObjectDescriptor child_script = GetComponentInChildren<ObjectDescriptor>();
+	AudioSource aSource;
+	ObjectDescriptor child_script;
 
 	// Use this for initialization
 	void Start () {
-
+		aSource = GetComponentInChildren<AudioSource>();
+		child_script = GetComponentInChildren<ObjectDescriptor>(); // Get the 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (active_playing) {
+				if (!aSource.audio.isPlaying) { // if the active_playing is set and audio is not playing
+					Finish ();
+			}
+		}
 	}
 
 	void OnTriggerEnter(Collider col) {
-		if (col.name == "First Person Controller") {
+		if (col.name == "First Person Controller" && !aSource.audio.isPlaying) {
 			Debug.Log("Player Collision Detected");
 			active_GUI = true;
+			active_playing = true;
+			aSource.audio.Play();
 		}
 	}
 
 	void OnTriggerExit(Collider col) {
-		//Debug.Log (col.name);
-		if (col.name  == "First Person Controller") {
-			active_GUI = false;
-		}
+		Debug.Log (col.name);
 	}
 
-	void DisplayText () {
-
+	void Finish() {
+		active_playing = false;
+		active_GUI = false;
 	}
 
 	void OnGUI() {
 		if (active_GUI) {
-			ObjectDescriptor child_script = GetComponentInChildren<ObjectDescriptor>(); // Get the 
 			string desc = child_script.desc.ToString();
 
 			//Debug.Log (desc);
