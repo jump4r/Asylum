b9a9437ac09c92e2ed0c5aa852a0f4fa4fb441b9
Andrew Morris <jump3r@dggaming.net>
2014-03-18 02:44:22 -0400

    Doors!
    Sliding and Rotating doors now in place. Tweaked the level. In actuality, we should start a new level with things actually in the right place. Right now this is turning into another test playground.

---
+++ Assets/Scripts/DoorRotate.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorRotate : MonoBehaviour {
+	private float smooth = 2.0f;
+	private float DoorOpenAngle = -95.0f;
+	private float DoorCloseAngle = 0.0f;
+	private bool open = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (open) {
+			// Lerp towards the target location.
+			Quaternion targetRotation = Quaternion.Euler(0, DoorOpenAngle, 0);
+			transform.localRotation = Quaternion.Slerp (transform.localRotation, targetRotation, Time.deltaTime * smooth);
+		}
+
+		if (!open) {
+			Quaternion targetRotation0 = Quaternion.Euler(0, DoorCloseAngle, 0);
+			transform.localRotation = Quaternion.Slerp (transform.localRotation, targetRotation0, Time.deltaTime * smooth);
+		}
+	}
+
+	void OnTriggerEnter(Collider col) {
+		open = true;
+	}
+
+	void OnTriggerExit(Collider col) {
+		open = false;
+	}
+}
---
+++ Assets/Scripts/DoorSlide.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorSlide : MonoBehaviour {
+	private float smooth = 3.0f;
+	private float doorSlideDist = 3.0f;
+	private bool open = false;
+	private bool lerping = true;
+	private Vector3 initPos;
+	
+	// Use this for initialization
+	void Start () {
+		initPos = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (open) {
+			// Lerp towards the target location.
+			Vector3 t1 = new Vector3(initPos.x + doorSlideDist, initPos.y, initPos.z); // Max Slide Dist
+			Vector3 t2 = new Vector3(transform.localPosition.x + Time.deltaTime * smooth, transform.localPosition.y, transform.localPosition.z);
+			float min = Mathf.Min (t1.x, t2.x);
+			transform.localPosition = new Vector3(min, transform.localPosition.y, transform.localPosition.z);
+		}
+		
+		if (!open) {
+			Vector3 t1 = new Vector3(initPos.x, initPos.y, initPos.z);
+			Vector3 t2 = new Vector3(transform.localPosition.x - Time.deltaTime * smooth, transform.localPosition.y, transform.localPosition.z);
+			float max = Mathf.Max (t1.x, t2.x);
+			transform.localPosition = new Vector3(max, transform.localPosition.y, transform.localPosition.z);
+		}
+	}
+	
+	void OnTriggerEnter(Collider col) {
+		open = true;
+	}
+	
+	IEnumerator OnTriggerExit(Collider col) {
+		yield return new WaitForSeconds (2); 
+		open = false;
+	}
+}
