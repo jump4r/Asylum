5c91a66d64bbfe55e44a4162f2d9fdb1521ea553
jump4r <jump3r@dggaming.net>
2014-03-23 16:19:51 -0400

    Collision added to level, Object Descriptor scripts changed to be more efficeint.

--- Assets/Scripts/ObjectAudioDescriptor.cs
+++ Assets/Scripts/ObjectAudioDescriptor.cs
@@ -1,39 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ObjectDescriptor : MonoBehaviour {
-
+public class ObjectAudioDescriptor : MonoBehaviour {
+	
 	public TextAsset desc;
 	// public AudioClip clip;
 	private bool active_GUI = false;
+	private bool active_playing = false;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		if (active_playing) {
+			if (!audio.isPlaying) { // if the active_playing is set and audio is not playing
+				Finish ();
+			}
+		}
 	}
+
 	void OnTriggerEnter(Collider col) {
-		if (col.name == "First Person Controller") {
+		if (col.name == "First Person Controller" && !audio.isPlaying) {
 			//Debug.Log("Player Collision Detected");
 			active_GUI = true;
+			active_playing = true;
+			audio.Play();
 		}
 	}
 	
 	void OnTriggerExit(Collider col) {
-		//Debug.Log (col.name);
-		if (col.name  == "First Person Controller") {
-			active_GUI = false;
-		}
+		Debug.Log (col.name);
 	}
 	
-	void DisplayText () {
-		
+	void Finish() {
+		active_playing = false;
+		active_GUI = false;
 	}
-	
+
 	void OnGUI() {
 		if (active_GUI) {		
 			Debug.Log (desc);
@@ -41,3 +47,5 @@ public class ObjectDescriptor : MonoBehaviour {
 		}
 	}
 }
+
+
