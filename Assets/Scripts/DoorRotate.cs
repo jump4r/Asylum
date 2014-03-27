047fc5c6c62deed33a5a31bdd92e014a34126202
jump4r <jump3r@dggaming.net>
2014-03-27 10:13:08 -0400

    Level Design Changes. Waiting for the FINAL final version of the map before I add collision again, as we already know that collision works. I'm hoping that the level design will be completed today.

--- Assets/Scripts/DoorRotate.cs
+++ Assets/Scripts/DoorRotate.cs
@@ -3,18 +3,18 @@ using System.Collections;
 
 public class DoorRotate : MonoBehaviour {
 	private float smooth = 2.0f;
-	private float DoorOpenAngle = -95.0f;
+	private float DoorOpenAngle = -135.0f;
 	private float DoorCloseAngle = 0.0f;
-	private bool open = false;
+	private bool open = false; 
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (open) {
+		 /* if (open) {
 			// Lerp towards the target location.
 			Quaternion targetRotation = Quaternion.Euler(0, DoorOpenAngle, 0);
 			transform.localRotation = Quaternion.Slerp (transform.localRotation, targetRotation, Time.deltaTime * smooth);
@@ -23,14 +23,25 @@ public class DoorRotate : MonoBehaviour {
 		if (!open) {
 			Quaternion targetRotation0 = Quaternion.Euler(0, DoorCloseAngle, 0);
 			transform.localRotation = Quaternion.Slerp (transform.localRotation, targetRotation0, Time.deltaTime * smooth);
-		}
+		}*/ 
 	}
 
 	void OnTriggerEnter(Collider col) {
-		open = true;
+		Debug.Log (animation.name);
+
+		// animation ["door_open"].wrapMode = WrapMode.Once;
+		if (!open) {
+			animation ["door_open"].speed = 1;
+			open = true;
+			animation.Play ("door_open");
+		}
 	}
 
-	void OnTriggerExit(Collider col) {
-		open = false;
-	}
+	 /* void OnTriggerExit(Collider col) {
+		Debug.Log ("Rewind animation");
+		animation ["door_open"].speed = -1f;
+		animation ["door_open"].time = animation ["door_open"].length;
+		animation.Play ("door_open");
+	} */
+	
 }
