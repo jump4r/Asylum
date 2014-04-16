5ffcadea789d69cff931c19e6f3589d461325dcd
Markus Gaasedelen <gaasem@rpi.edu>
2014-04-16 07:26:56 -0400

    Did a bit more work on the tiles, implemented the rotating functionality. Still need to add the stuff to trigger it. Will do in class.

---
+++ Assets/Scripts/TetrisTile.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TetrisTile : MonoBehaviour {
+
+	public bool rotating = false;
+	public bool rotated = true;
+	private float rotateSpeed = 1.0f;
+	private float rotateProgress = 0.0f;
+	private const float toRotate = 90.0f;
+	Transform tile;
+
+	void Awake()
+	{
+		tile = transform.FindChild("tile_pieces");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(rotating)
+		{
+			rotateProgress += rotateSpeed * Time.deltaTime;
+			tile.transform.Rotate(new Vector3(0, rotateSpeed*Time.deltaTime, 0));
+
+			if(rotateProgress >= toRotate)
+			{
+				rotating = false;
+				rotated = true;
+			}
+		}
+	}
+}
