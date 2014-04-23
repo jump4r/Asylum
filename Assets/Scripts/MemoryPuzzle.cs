cbaef5ab5ea4d12cee4c14afad477cbc5aa7f349
Nicholas Payne <nicholasfpayne@gmail.com>
2014-04-23 07:43:26 -0400

    Memory Puzzle implemented.

--- Assets/Scripts/MemoryPuzzle.cs
+++ Assets/Scripts/MemoryPuzzle.cs
@@ -7,44 +7,120 @@ public class MemoryPuzzle : MonoBehaviour
 	public Material goodTile;
 	public Material blankTile;	
 	public Material endTile;
+	public Material invisTile;
 	public GameObject glowingTile;
 	public int gridX = 7;
 	public int gridY = 7;
-	private float spacing = 5f;
-	private int startX;
-	private int startY;
-	private int direction;
-	private bool pathFound;
 	public int difficultyLevel = 5;
-	int[,] grid;
-	
+	public bool puzzleStarted = false;
+
+	private GameObject startTile, player;
+	private float spacing = 5f;
+	private int startX, startY, direction;
+	private bool pathFound;
+	private GameObject [,] gridTiles;
+	private int[,] grid;
+	private float startTime;
+	private int firstX, firstY;
+	private Vector3 startPos, endPos;
+	private float speed = 0;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+		grid = new int [gridX,gridY];
+		gridTiles = new GameObject[gridX, gridY];
+	}
+
 	void Start()
 	{
-		grid = new int [gridX,gridY];
 		FindPath ();
+		MakeGrid ();
+		MakeStartPlatform ();
+	}
+
+	void Update()
+	{
+		CheckForPuzzleStart ();
+		MoveStartPlatform ();
+		CheckPlayerHeight ();
+	}
+
+	void MoveStartPlatform ()
+	{
+		if (startTile.transform.position.y >= 0)
+		{
+			speed = (Time.time - startTime) / (difficultyLevel / 2.0f);
+			startTile.transform.position = new Vector3(Mathf.SmoothStep(startPos.x, endPos.x, speed), Mathf.SmoothStep(startPos.y, endPos.y, speed/2), Mathf.SmoothStep(startPos.z, endPos.z, speed));
+			player.transform.position = startTile.transform.position+transform.up;
+		}
+	}
+
+	void CheckPlayerHeight()
+	{
+		if(player.transform.position.y < -10)
+		{
+			player.transform.position = endPos + transform.up * 10;
+		}
+	}
+
+	void CheckForPuzzleStart()
+	{
+		if ((Time.time - startTime > difficultyLevel && puzzleStarted == false) || (Time.time - startTime < difficultyLevel && puzzleStarted == true))
+		{
+			puzzleStarted = true;
+			for (int y = 0; y < gridY; y++)
+			{
+				for (int x = 0; x < gridX; x++)
+				{
+					gridTiles[x,y].renderer.material = blankTile;
+					gridTiles[x,y].GetComponentInChildren<PuzzleTile>().puzzleStarted = true;
+				}
+			}
+		}
+	}
+
+	void MakeStartPlatform()
+	{
+		float xStart = gridX / 2.0f * spacing - spacing * 0.5f;
+		float yStart = spacing * -2;
+		float mag = Mathf.Sqrt(Mathf.Pow ((firstX - xStart), 2) + Mathf.Pow ((firstY - yStart), 2));
+		startPos = new Vector3(xStart, mag * 2, yStart);
+		glowingTile.renderer.material = invisTile;
+		glowingTile.tag = "";
+		startTile = Instantiate(glowingTile, startPos, Quaternion.identity) as GameObject;
+		startTime = Time.time;
+		endPos = new Vector3 (firstX, -0.05f, firstY) * spacing;
+	}
+
+	void MakeGrid()
+	{
 		for (int y = 0; y < gridY; y++)
 		{
 			for (int x = 0; x < gridX; x++)
 			{
-				Vector3 pos = new Vector3(x, 0.01f, y) * spacing;
-
+				Vector3 pos = new Vector3(x, 0, y) * spacing;
+				
 				if (grid[x,y] == 1)
 				{
 					glowingTile.renderer.material = goodTile;
+					glowingTile.tag = "GoodTile";
 				}
 				else if (grid[x,y] == 2)
 				{
 					glowingTile.renderer.material = endTile;
+					glowingTile.tag = "GoalTile";
 				}
 				else
 				{
 					glowingTile.renderer.material = badTile;
+					glowingTile.tag = "BadTile";
 				}
-				Instantiate(glowingTile, pos, Quaternion.identity);
+				gridTiles [x,y] = Instantiate(glowingTile, pos, Quaternion.identity) as GameObject;
 			}
 		}
-	} 
-
+	}
+	
 	void FindPath()
 	{
 		if (Random.Range (0, 2) == 1)
@@ -72,16 +148,18 @@ public class MemoryPuzzle : MonoBehaviour
 			}
 		}
 		grid [startX, startY] = 1;
+		firstX = startX;
+		firstY = startY;
 
 		for (int i = 0; i < difficultyLevel; i++)
 		{
-			Debug.Log("Finding path");
+			//Debug.Log("Finding path");
 			pathFound = false;
 			direction = Random.Range (0,4);
 			int j = 0;
 			while (pathFound == false && j < 4)
 			{
-				Debug.Log("Finding path");
+				//Debug.Log("Finding path");
 				if (direction == 0)
 				{
 					if (startY < gridY - 2 && startX < gridX-1 && startX > 0 && grid[startX-1,startY+1] == 0 && grid[startX,startY+2] == 0 && grid[startX+1,startY+1] == 0)
---
+++ Assets/Scripts/PuzzleTile.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzleTile : MonoBehaviour 
+{
+	public Texture endTile;
+	public Texture badTile;
+	public bool puzzleStarted = false;
+
+	void OnTriggerEnter(Collider collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			if (transform.parent.tag == "GoodTile")
+			{
+				GameObject.FindGameObjectWithTag("GameController").GetComponent<MemoryPuzzle>().puzzleStarted = true;
+			}
+			if (transform.parent.tag == "BadTile")
+			{
+				StartCoroutine(ResetLevel());
+			}
+			else if (transform.parent.tag == "GoalTile")
+			{
+				//What to do when you win goes here.
+			}
+		}
+	}
+
+	IEnumerator ResetLevel()
+	{
+		//play fail sound
+		GameObject.FindGameObjectWithTag ("Player").GetComponent<CharacterMotor> ().enabled = false;
+		yield return new WaitForSeconds(3);
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
