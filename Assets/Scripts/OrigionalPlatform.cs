1521419c1c8b50ef4579926ab0f3fe56fc833c45
pgaret <peregringaret@gmail.com>
2014-04-25 00:32:36 -0400

    Fixed the perma-reloading First Person Control bug for David's puzzle.  :>

--- Assets/Scripts/OrigionalPlatform.cs
+++ Assets/Scripts/OrigionalPlatform.cs
@@ -7,7 +7,7 @@ public class OrigionalPlatform : MonoBehaviour {
 	public float speed = .2f;
 	Vector3 downdest = new Vector3(0,0,0);
 	float origionaltime = 0.0f;
-	public GameObject IAMLITERALLYFUNCTIONALLYRETARDED;
+	public GameObject[] IAMLITERALLYFUNCTIONALLYRETARDED;
 	Vector3 death = new Vector3(0,0,0);
 
 	// Use this for initialization
@@ -15,8 +15,14 @@ public class OrigionalPlatform : MonoBehaviour {
 		var otherpos = transform.position;
 		downdest.x = otherpos.x;
 		downdest.z = otherpos.z;
+		death.y = otherpos.y;
+		IAMLITERALLYFUNCTIONALLYRETARDED = GameObject.FindGameObjectsWithTag("Player");
 
-		IAMLITERALLYFUNCTIONALLYRETARDED = GameObject.Find("First Person Controller");
+		for (int i = 0; i < IAMLITERALLYFUNCTIONALLYRETARDED.Length; i++)
+		{
+			if (IAMLITERALLYFUNCTIONALLYRETARDED[i].transform.position.y < 640) Destroy (IAMLITERALLYFUNCTIONALLYRETARDED[i]);
+		}
+	
 
 	}
 	
@@ -28,11 +34,12 @@ public class OrigionalPlatform : MonoBehaviour {
 			transform.position = Vector3.MoveTowards(transform.position, downdest, speed); //goes straight down
 		if(transform.position.y <= 930)
 			speed = .1f;
-		var otherpos = IAMLITERALLYFUNCTIONALLYRETARDED.transform.position;
-		death.y = otherpos.y;
+		death.y = IAMLITERALLYFUNCTIONALLYRETARDED [IAMLITERALLYFUNCTIONALLYRETARDED.Length - 1].transform.position.y;
 
 		if(death.y <= 630) {
 			Application.LoadLevel("thescene");
+			Debug.Log ("LOADLEVEL");
+
 		}
 	}
 }
