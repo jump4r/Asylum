4bda2b3dd858c76c59c85a0f4f92a72261b8316b
pgaret <peregringaret@gmail.com>
2014-05-07 12:32:56 -0400

    Smoke Weed Everyday

---
+++ Assets/Scripts/Footprints.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Footprints : MonoBehaviour {
+
+	bool soundplaying = false;
+	CharacterController playerscript;
+	// Use this for initialization
+	void Start () {
+
+		playerscript = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerscript.velocity.magnitude > 1 && playerscript.isGrounded == true && soundplaying == false) {
+			audio.Play ();
+			soundplaying = true;
+
+		} 
+		else if(playerscript.velocity.magnitude < 1 || playerscript.isGrounded == false) {
+			audio.Stop();
+			soundplaying = false;
+		}
+
+	}
+
+
+}
